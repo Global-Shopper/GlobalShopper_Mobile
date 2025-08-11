2bfc397483f19eca34877c8669b13211ac682c26
Phuong Dao Hoai <daophuong131.work@gmail.com>
2025-08-11 15:03:13 +0700

    fix: upload img cloundinary

---
+++ src/const/urlconst.js
@@ -0,0 +1,4 @@
+export const CLOUDINARY_NAME = import.meta.env.VITE_CLOUDINARY_CLOUD_NAME;
+export const CLOUDINARY_UPLOAD_PRESET = import.meta.env
+	.VITE_CLOUDINARY_UPLOAD_PRESET;
+export const REDIRECT_URI = import.meta.env.VITE_REDIRECT_URI;
